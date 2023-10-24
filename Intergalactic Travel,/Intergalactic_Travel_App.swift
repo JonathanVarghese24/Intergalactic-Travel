75ab311351ffbb3e0f90576c8b397eb1a68ed667
Jonathan Varghese <jonathanvarghese24@gmail.com>
2023-10-24 09:46:27 -0500

    Initial Commit

---
+++ Intergalactic Travel,/Intergalactic_Travel_App.swift
@@ -0,0 +1,17 @@
+//
+//  Intergalactic_Travel_App.swift
+//  Intergalactic Travel,
+//
+//  Created by Jonathan Varghese on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Intergalactic_Travel_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
